a6542d8f217e88a11fd80572ec270a07fda365f3
andresbenito <andres.benito@alumnos.upm.es>
2024-10-09 21:09:15 +0200

    restructuracion del main

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from auth import authenticate_gmail
 from core.mail import process_emails, categorize_func, get_gmail_labels
 
-
-if __name__ == '__main__':
-    
-    
+def main():
     service = authenticate_gmail()
-    labels= get_gmail_labels(service)
+    labels = get_gmail_labels(service)
     label_names = [label['name'] for label in labels]
     process_emails(service, categorize_func,label_names)
+
+
+if __name__ == '__main__':
+    main()
